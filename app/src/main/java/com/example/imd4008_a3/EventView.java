361b01b10be463fd750666951d7485550556ce98
Ethan McIntyre <EthanMcIntyre@cmail.carleton.ca>
2022-11-21 13:55:22 -0500

    woooo

---
+++ app/src/main/java/com/example/imd4008_a3/EventView.java
@@ -0,0 +1,13 @@
+package com.example.imd4008_a3;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+
+public class EventView extends CardView {
+
+    public EventView(@NonNull Context context) {
+        super(context);
+    }
+}
--- app/src/main/java/com/example/imd4008_a3/MainActivity.java
+++ app/src/main/java/com/example/imd4008_a3/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.imd4008_a3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ScrollView todoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        todoView = findViewById(R.id.todoView);
+
+        //todoView.addView(new CardView);
     }
 }
