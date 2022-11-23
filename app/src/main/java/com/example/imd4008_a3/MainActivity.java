eb70820ac83eb0c0ea28544face7a8ba2ba31dd3
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2022-11-23 11:40:19 -0500

    starting on the fragment switcher

--- app/src/main/java/com/example/imd4008_a3/MainActivity.java
+++ app/src/main/java/com/example/imd4008_a3/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.imd4008_a3;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.os.Build;
 import android.os.Bundle;
@@ -12,6 +14,8 @@ import android.widget.LinearLayout;
 import android.widget.Space;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -19,12 +23,21 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout todoView;
     List<Task> taskList;
+    FloatingActionButton addFab;
+    FragmentContainerView fcv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        fcv = findViewById(R.id.nav_fragment_host);
+        addFab = findViewById(R.id.addFab);
+        addFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
